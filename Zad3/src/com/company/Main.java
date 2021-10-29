7bdaec85d816c65fdaafec6964b7c4cf093c394f
GhostGamesV11 <47499005+GhostGamesV11@users.noreply.github.com>
2021-10-29 06:56:12 +0200

    Add files via upload

---
+++ Zad3/src/com/company/Main.java
@@ -0,0 +1,266 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Random;
+
+// 1 a)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int parz = 0;
+//        int niep = 0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 +999)-999);
+//            if(tab[i] % 2 == 0)parz++;
+//            if(tab[i] % 2 == 1)niep++;
+//        }
+//        System.out.print("Liczby parzyste: "+parz+"\nLiczby nieparzyste: "+niep);
+//    }
+//}
+
+// b)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int dod = 0;
+//        int uj = 0;
+//        int zero = 0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 + 999))-999;
+//            if(tab[i] > 0)
+//                dod++;
+//            if(tab[i] < 0)
+//                uj++;
+//            if(tab[i] == 0)
+//                zero++;
+//        }
+//        System.out.print("Liczby dodatnie: "+dod+"\nLiczby ujemne: "+uj+"\nIlość zer: "+zero);
+//    }
+//}
+
+// c)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int max = 0;
+//        int ilosc = 0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 + 999))-999;
+//            if(tab[i]>max)
+//                max=tab[i];
+//        }
+//        for(int i=0;i<n;i++)
+//        {
+//            if(tab[i]==max)
+//                ilosc++;
+//        }
+//        System.out.print("Największa liczba to: "+max+"\n Występuje ona "+ilosc+" razy");
+//    }
+//}
+
+// d)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int dod = 0;
+//        int uj = 0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 + 999))-999;
+//            if(tab[i] > 0)
+//                dod+=tab[i];
+//            if(tab[i] < 0)
+//                uj+=tab[i];
+//
+//
+//        }
+//        System.out.print("Suma liczb dodatnich: "+dod+"\nSuma liczb ujemnych: "+uj);
+//    }
+//}
+
+// e)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int ciag = 0;
+//        int wynik=0;
+//        int max_ciag=0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 + 999))-999;
+//            if(tab[i] > 0 )
+//            {
+//                ciag++;
+//                if (ciag>max_ciag)
+//                    max_ciag=ciag;
+//            }
+//            else
+//                ciag=0;
+//
+//
+//
+//        }
+//        System.out.print("Najdluzszy fragment tablicy zawierający tylko dodatnie liczby to: "+max_ciag);
+//    }
+//}
+
+
+// f)
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        Random r = new Random();
+//        int ciag = 0;
+//
+//        int max_ciag=0;
+//        int n = 0;
+//        int x = 0;
+//        while(x!=1){
+//
+//            System.out.print("Podaj n z przedziału od 1 do 100: ");
+//            n = scan.nextInt();
+//            if(n >=1 && n <= 100)
+//                x = 1;
+//        }
+//        int tab[] = new int[n];
+//        for(int i = 0;i < n;i++){
+//            tab[i] = r.nextInt((999 + 999))-999;
+//            if(tab[i] < 0)
+//            {
+//                tab[i]=-1;
+//
+//            }
+//            if(tab[i] > 0)
+//            {
+//                tab[i]=1;
+//            }
+//            System.out.print(tab[i]+"\n");
+//
+//
+//        }
+//
+//    }
+//}
+
+
+public class Main {
+
+    public static void generuj(int tab[], int n, int minWartosc, int maxWartosc) {
+        Random r = new Random();
+        int x = 0;
+        while (x != 1) {
+            if (n >= 1 && n <= 100)
+                x = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            tab[i] = r.nextInt((maxWartosc + maxWartosc)) + minWartosc;
+
+            System.out.print(tab[i] + "\n");
+        }
+    }
+
+    public static int ileNieparzystych(int tab[]) {
+        int niep = 0;
+        for(int i=0;i<tab.length;i++) {
+            if (tab[i] % 2 != 0)
+                niep++;
+
+        }
+        return niep;
+    };
+
+
+    public static int ileParzystych(int tab[])
+    {
+    int parz = 0;
+
+            for(int i=0;i<tab.length;i++){
+                if(tab[i]%2==0)
+                    parz++;
+
+            }
+
+        return parz;
+};
+    public static int ileDodatnich(int tab[])
+    {
+        int dod=0;
+        for(int i=0;i<tab.length;i++)
+        {
+            if(tab[i]>0)
+                dod++;
+        }
+        return dod;
+    };
+    public static void main(String[] args){
+        int n=0;
+        int x=0;
+        while(x!=1){
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Podaj n z przedziału od 1 do 100: ");
+            n = scan.nextInt();
+            if(n >=1 && n <= 100)
+                x = 1;
+        }
+        int tab[] = new int[n];
+        generuj(tab,n,-999,999);
+        System.out.print("Ilość nieparzystych liczb: " + ileNieparzystych(tab)+"\n");
+        System.out.print("Ilość parzystych liczb: " + ileParzystych(tab)+"\n");
+        System.out.print("Ilość dodatnich liczb: " + ileDodatnich(tab)+"\n");
+    }
+}
+
+
+
